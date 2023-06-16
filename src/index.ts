fb6767cfa46c3450448999bb6faacbb754a12e22
mateushlsilva <mateushls01@gmail.com>
2023-06-16 15:54:59 -0300

    Adiciona cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 import express = require("express");
+import * as dotenv from 'dotenv';
+import cors = require("cors");
+
+
+dotenv.config();
 
 const app = express();
 
-const PORT = 3001
+
+app.use(cors())
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
+
+
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
